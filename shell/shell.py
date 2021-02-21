9d3ec030ae2b5755fe760f66bd7e09cad1f20bdf
emwhite3 <emwhite3@miners.utep.edu>
2021-02-21 10:21:52 -0700

    added ps1 support, output redirect support and start to myReadline

--- shell/shell.py
+++ shell/shell.py
@@ -2,8 +2,17 @@
 
 import os, sys, re
 
+def check_ps1():
+  if len(os.environ['PS1']) < 20 and len(os.environ['PATH'] > 0):
+    return os.environ['PS1']
+  return '$ '
+
+def my_readline(fd):
+  while True:
+    return
+
 while 1:
-  os.write(1, '$ '.encode())
+  os.write(1, check_ps1().encode())
   input = os.read(0, 1000)
 
   if input.decode('utf-8')[0:4] == 'exit':
@@ -20,6 +29,12 @@ while 1:
     # os.write(1, 'Child!\n'.encode())
     
     args = re.split(' ', input.decode('utf-8')[0:-1])
+    if '>' in args:
+      os.close(1)
+      os.open(args[(args.index('>')+1)], os.O_CREAT | os.O_WRONLY)
+      os.set_inheritable(1, True)
+      args = args[:args.index('>')]
+
     for dir in re.split(":", os.environ['PATH']):
       program = "%s/%s" % (dir, args[0])
       # os.write(1, 'Trying to execute...\n'.encode())
@@ -28,7 +43,7 @@ while 1:
       except FileNotFoundError:
         pass
       
-    os.write(2, ('Could not find \'%s\' program to execute'%args[0]).encode())
+    os.write(2, ('Could not find \'%s\' program to execute\n'%args[0]).encode())
     sys.exit(1)
 
 
