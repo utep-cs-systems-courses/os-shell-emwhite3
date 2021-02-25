1e37e424d987a221c49130b8e73665a37f601457
emwhite3 <emwhite3@miners.utep.edu>
2021-02-24 18:47:13 -0700

    added pipe support

--- shell/shell.py
+++ shell/shell.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python3
 
-import os, sys, re
+import os, sys, re, time
 
 
 # check if the set ps1 is set and not too long
 def check_ps1():
-  if len(os.environ['PS1']) < 20 and len(os.environ['PS1']) >= 0:
-    return os.environ['PS1']
+  try:
+    if len(os.environ['PS1']) < 20 and len(os.environ['PS1']) >= 0:
+      return os.environ['PS1']
+  except KeyError:
+    return '$ '
   return '$ '
 
 
@@ -39,10 +42,11 @@ def execute_args(args):
 # they are modified in the loop
 args = []
 pipe = False
-
 while 1:
   amper = False
-  if not pipe:			# these are the checks for 'special' commands
+  print('top of loop', args)
+  print('pid', os.getpid())
+  if not pipe:			      # these are the checks for 'special' commands
     args = my_read_line()
   if args[0] == 'exit':
     os.write(1, 'Goodbye!\n'.encode())
@@ -57,54 +61,57 @@ while 1:
     pr, pw = os.pipe()
     for fd in (pr, pw):
       os.set_inheritable(fd, True)
-  				# fork here to the child process with gathered information
-  rc = os.fork()
+  				                  
+  rc = os.fork()            # fork here to the child process with gathered information
   
-  if rc < 0:			# if the fork failed notify the user
+  if rc < 0:			          # if the fork failed notify the user
     os.write(2, 'Fork failed!\n'.encode())
     sys.exit(0)
-  elif rc == 0:			# this is the child process
+  elif rc == 0:			        # this is the child process
     if pipe or '|' in args:	# check if we are a reading writing or both pipe command
-      if pipe:			# set appropriate fds for reading and writing to a pipe
+      if pipe:			        # set appropriate fds for reading and writing to a pipe
+        #print(os.read(pr, 10000))
+        print(args)
+        print('in last command')
         os.close(0)
         dup_r = os.dup(pr)
+        os.close(pr)
         os.set_inheritable(dup_r, True)
       if '|' in args:
         os.close(1)
         dup_w = os.dup(pw)
+        os.close(pw)
         os.set_inheritable(dup_w, True)
         args = args[:args.index('|')]
-      for fd in (pr, pw):
-        os.close(fd)
       
-    elif '>' in args:		# check if we have output redirect and set fds
+      
+      
+    elif '>' in args:		      # check if we have output redirect and set fds
       os.close(1)
       os.open(args[(args.index('>')+1)], os.O_CREAT | os.O_WRONLY)
       os.set_inheritable(1, True)
       del args[args.index('>')+1]
       del args[args.index('>')]
-    elif '<' in args:		# check if we have input redirect and set fds
+    elif '<' in args:		      # check if we have input redirect and set fds
       os.close(0)
       os.open(args[(args.index('<')+1)], os.O_RDONLY)
       os.set_inheritable(0, True)
       del args[args.index('<')+1]
       del args[args.index('<')]
-      
-    execute_args(args)		# execute the child process
+    
+    execute_args(args)		    # execute the child process
 
-  elif amper:			# parent checks
-    pass			# if we want a background process do not wait for child to finish
-  elif '|' in args:		# if we are a write process set args for next child process
+  elif amper:			            # parent checks
+    pass			                # if we want a background process do not wait for child to finish
+  elif '|' in args:		        # if we are a write process set args for next child process
     os.wait()
     args = args[(args.index('|')+1):]
+    print("passing last args: ", args)
     pipe = True
-  elif pipe and not ('|' in args):
-    pipe = False		# if we are an ending read pipe process output to user
-    print(os.read(pr, 10000).decode())
-    for fd in (pw, pr):
-      os.close(fd)
-  
-  else:				# wait for child process to finish
-   os.wait()
-    # os.write(1, 'Parent!\n'.encode())
-  # os.write(1, input)
+  elif pipe:
+    #print(os.read(pr, 10000).decode())
+    os.wait()
+    print('in pipe elif')
+    pipe = False
+  else:				                # wait for child process to finish
+    os.wait()
